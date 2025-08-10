78d4aa24978cd3283215f1a971ae13f5953b97c4
Mohammed-Zeghlache <mouhamedzeghlache7@gmail.com>
2025-08-10 23:37:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173', // for development
     // 'https://opticien-tayeb-bey-msila.netlify.app' 
-   'https://tayeb-bay-optic.netlify.app'// for production
+   'https://tayeb-bay-msila.netlify.app'// for production
   ]
 }));
 
